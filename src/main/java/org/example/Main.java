8dc96a2208074e4745bb8da0dbaa46dc8e32539c
Arsalan <arsik.14.12@mail.ru>
2026-01-30 19:03:14 +0800

    первоначальный код

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        String[] columnMapping = {"id", "firstName", "lastName", "country", "age"};
+        String fileName = "data.csv";
+    }
+}
